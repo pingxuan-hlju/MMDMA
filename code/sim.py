d9fd46ebf772b981bdce476b865f3ca9ce8e6f14
pingxuan-hlju <48944768+pingxuan-hlju@users.noreply.github.com>
2025-07-31 22:18:13 +0800

    Update sim.py

--- code/sim.py
+++ code/sim.py
@@ -15,3 +15,22 @@ def calculate_sim_l(ddad, ds):
     return s
 s = calculate_sim_l(ddad, ds)
 np.savetxt('./data/drughesimilarity.txt', s)
+
+mmad = torch.from_numpy(np.loadtxt('./data/microbe_microbe_similarity.txt'))  
+interaction = torch.from_numpy(loadmat('./data/net1.mat')['interaction'])  
+ms = interaction.T  
+
+def calculate_sim_m(mmad, ms, sigma=1.0):
+    mat = torch.cat([mmad, ms], dim=1)  # 173 x (173 + 1373)
+    mat_np = mat.numpy()
+    s = np.zeros((mat_np.shape[0], mat_np.shape[0]))
+    for i in range(mat_np.shape[0]):
+        for j in range(mat_np.shape[0]):
+            dist = np.linalg.norm(mat_np[i] - mat_np[j])
+            s[i, j] = np.exp(-dist ** 2 / (2 * sigma ** 2))
+    return s
+
+sigma_value = 1.0 
+s_mic = calculate_sim_m(mmad, ms, sigma=sigma_value)
+
+np.savetxt('./data/newmicrobesimilarity.txt', s_mic)
